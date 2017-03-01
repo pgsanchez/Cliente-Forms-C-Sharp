acae022b6ba7b16030941a29a7e90db7fcb3f148
pgsanchez <pedrogonzalez.sanchez@gmail.com>
2017-03-01 17:14:19 +0100

    Subida inicial
    Cliente básico. Hecho en Visual C#. Utiliza windows Forms. Con sockets.
    El proyecto está completo para bajarlo, abrirlo con Visual C#, compilar y ejecutar.

---
+++ ClienteForms/ClienteForms/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace ClienteForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxMsgs = new System.Windows.Forms.TextBox();
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.btnDesconectar = new System.Windows.Forms.Button();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxMsgs
+            // 
+            this.textBoxMsgs.Location = new System.Drawing.Point(27, 79);
+            this.textBoxMsgs.Name = "textBoxMsgs";
+            this.textBoxMsgs.Size = new System.Drawing.Size(225, 20);
+            this.textBoxMsgs.TabIndex = 0;
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.Location = new System.Drawing.Point(105, 38);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(75, 23);
+            this.btnConectar.TabIndex = 1;
+            this.btnConectar.Text = "Conectar";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // btnDesconectar
+            // 
+            this.btnDesconectar.Location = new System.Drawing.Point(90, 227);
+            this.btnDesconectar.Name = "btnDesconectar";
+            this.btnDesconectar.Size = new System.Drawing.Size(90, 23);
+            this.btnDesconectar.TabIndex = 2;
+            this.btnDesconectar.Text = "Desconectar";
+            this.btnDesconectar.UseVisualStyleBackColor = true;
+            this.btnDesconectar.Click += new System.EventHandler(this.btnDesconectar_Click);
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Location = new System.Drawing.Point(105, 105);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(75, 23);
+            this.btnEnviar.TabIndex = 3;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.btnDesconectar);
+            this.Controls.Add(this.btnConectar);
+            this.Controls.Add(this.textBoxMsgs);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxMsgs;
+        private System.Windows.Forms.Button btnConectar;
+        private System.Windows.Forms.Button btnDesconectar;
+        private System.Windows.Forms.Button btnEnviar;
+    }
+}
+
---
+++ ClienteForms/ClienteForms/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ClienteForms
+{
+    public partial class Form1 : Form
+    {
+        Socket socket1;
+        IPEndPoint direccion;
+
+        public Form1()
+        {
+            InitializeComponent();
+            textBoxMsgs.Text = "Iniciando conexión...";
+            socket1 = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            direccion = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234);
+
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                socket1.Connect(direccion);
+                textBoxMsgs.Text = "Cliente conectado con éxito";
+            }
+            catch (Exception error)
+            {
+                // escribir en el msgbox que ha habido un error
+                textBoxMsgs.Text = "Error de conexión: " + error.ToString();
+            }
+        }
+
+        private void btnDesconectar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                socket1.Close();
+                textBoxMsgs.Text = "Cliente desconectado";
+            }
+            catch (Exception error)
+            {
+                // escribir en el msgbox que ha habido un error
+                textBoxMsgs.Text = "Error al desconectar: " + error.ToString();
+            }
+        }
+
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+            byte[] txtAEnviar;
+	        txtAEnviar = Encoding.Default.GetBytes("Hola, soy el cliente");
+            socket1.Send(txtAEnviar);
+            textBoxMsgs.Text = "Texto de prueba enviado";
+
+            // Esperamos a recibir algo
+            byte[] byARecibir = new byte[255];
+            int longitud = socket1.Receive(byARecibir);
+            Array.Resize(ref byARecibir, longitud);
+            //Console.WriteLine(Encoding.Default.GetString(byARecibir));
+            textBoxMsgs.Text = Encoding.Default.GetString(byARecibir);
+        }
+    }
+}
